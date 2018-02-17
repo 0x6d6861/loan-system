2402a0c77dae24259607424d51d9d8f2a226595e
Heri Agape <agape@live.fr>
2018-02-17 10:23:08 +0300

    added payment Model

--- app/Payment.php
+++ app/Payment.php
@@ -5,7 +5,6 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Transaction;
-use App\User;
 
 class Payment extends Model
 {
@@ -14,8 +13,5 @@ class Payment extends Model
     public function transaction()
     {
         return $this->belongsTo('Transaction');
-    }
-    public function user(){
-        return $this->belongsTo('User');
     }
 }
